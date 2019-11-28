43e0ac43bd4dcaedd1bcbac35ea1d378c131ca37
Bruna Vieira <bruna.vieira@supplylabs.com.br>
2019-11-28 10:35:44 -0300

    Pesquisar

--- TodoList/src/components/Card/Card.js
+++ TodoList/src/components/Card/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
      const { price, title, img } = props.prod;
   return (
         <div className="card">
-        <img src={img} className="card-img">
+        <img src={img} className="card-img" alt={title}>
         </img>
         <p>{title}</p>
         <p>{price}</p>
--- TodoList/src/pages/Produtos.js
+++ TodoList/src/pages/Produtos.js
@@ -1,76 +1,53 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import Card from '../components/Card/Card';
+import Input from '../components/Input/Input';
+import Button from '../components/Button/Button';
+import listaProdutos from '../mock/produtos';
 
-const produtos = [
-    {
-        id: 1,
-        title: "Notebook Lenovo",
-        img: "https://images-submarino.b2w.io/produtos/01/00/offers/01/00/item/133799/8/133799830SZ.png",
-        price: "29,90"
-    },
-    {
-        id: 2,
-        title: "Relógio ",
-        img: "",
-        price: "299,90"
-    },
-    {
-        id: 3,
-        title: "Fifa 19",
-        img: "",
-        price: "29,90"
-    },
-    {
-        id: 4,
-        title: "Fifa 19",
-        img: "",
-        price: "29,90"
-    },
-    {
-        id: 5,
-        title: "Fifa 19",
-        img: "",
-        price: "29,90"
-    },
-    {
-        id: 6,
-        title: "Fifa 19",
-        img: "",
-        price: "29,90"
-    },
-    {
-        id: 7,
-        title: "Fifa 19",
-        img: "",
-        price: "29,90"
-    },
-    {
-        id: 8,
-        title: "Fifa 19",
-        img: "",
-        price: "29,90"
-    },
-    {
-        id: 9,
-        title: "Fifa 19",
-        img: "",
-        price: "29,90"
-    },
-    {
-        id: 10,
-        title: "Fifa 19",
-        img: "",
-        price: "29,90"
+class Produtos extends Component {
+    constructor() {
+        super();
+        this.state = {
+            value: "",
+            produtos: listaProdutos
+        }
     }
-]
-const Produtos = props => (
-    <div className="lista_item">
-    {produtos.map(item => (
-        <Card 
-        key={item.id}
-        prod={item}/>
-    ))}
-    
-    </div>
-);
+
+    valorInput = (param) => {
+       // let valor = param.target.value;
+        this.setState({ 
+            value: param.target.value
+        })
+       // console.log(this.state.value)
+    }
+
+    pesquisar = () => {
+       let novoArray = this.state.produtos.filter(val => val.title.includes(this.state.value));
+       this.setState({
+           produtos: novoArray
+       })
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Input
+                placeholder="Pesquise seu produto"
+                tipo="text"
+                pegarValorInput={this.valorInput}
+                ></Input>
+                <Button
+                click={this.pesquisar}
+                >Pesquisar</Button>
+                <div className="lista_item">
+                    {this.state.produtos.map(item => (
+                        <Card
+                            key={item.id}
+                            prod={item} />
+                    ))}
+                </div>
+            </Fragment>
+        )
+    }
+};
 export default Produtos;
